44666feba882ec80bf7a8d74434c4069b8fc52a6
Douglas Damasceno <douglasmdamasceno@gmail.com>
2021-09-05 20:08:39 -0300

    createsession service

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -15,7 +16,12 @@ class CreateUserService {
         if (emailExists) {
             throw new AppError('Email address already used');
         }
-        const user = usersRepository.create({ name, email, password });
+        const hashedPassword = await hash(password, 8);
+        const user = usersRepository.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
         await usersRepository.save(user);
 
         return user;
