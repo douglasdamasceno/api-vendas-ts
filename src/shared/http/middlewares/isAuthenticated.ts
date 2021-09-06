56ba1040db8e2cd5c3f06c52e42744d5e2c7dd3a
Douglas Damasceno <douglasmdamasceno@gmail.com>
2021-09-05 22:48:06 -0300

    finalizando ajuste autentificação e configurando multer

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
-interface TokenPayLoad {
+interface ITokenPayLoad {
     iat: number;
     exp: number;
     sub: string;
@@ -23,7 +23,7 @@ export default function isAuthenticated(
     try {
         const decodedToken = verify(token, authConfig.jwt.secret);
 
-        const { sub } = decodedToken as TokenPayLoad;
+        const { sub } = decodedToken as ITokenPayLoad;
 
         request.user = {
             id: sub,
