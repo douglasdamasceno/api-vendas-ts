5e1ebdfe673e74840cbdc35b2f510c52125ce094
Douglas Damasceno <douglasmdamasceno@gmail.com>
2021-09-05 23:29:24 -0300

    rota upload do avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -3,8 +3,15 @@ import { celebrate, Joi, Segments } from 'celebrate';
 import UsersController from '../controllers/UserController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
 
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import UsersAvatarController from '../controllers/UserAvatarController';
+
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.post(
@@ -19,4 +26,10 @@ usersRouter.post(
     usersController.create,
 );
 
+usersRouter.patch(
+    '/avatar',
+    isAuthenticated,
+    upload.single('avatar'),
+    usersAvatarController.update,
+);
 export default usersRouter;
